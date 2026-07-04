7d8c0dd3111a9697a1cc5d2e804f55332f073ca7
Natalia Suszanowicz <nsuszano@c4r1s3.42warsaw.pl>
2026-07-04 19:35:49 +0200

    parser + header + makefile + push_swap.c

---
+++ parser.c
@@ -0,0 +1,13 @@
+#include "push_swap.h"
+
+void	parse_input(int argc, char **argv)
+{
+	int	i;
+	char	**input;
+
+	i = 1;
+	while (i < argc)
+	{
+		
+	}
+}
---
+++ push_swap.h
@@ -0,0 +1,18 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef enum e_algorithm
+{
+	NOT_SET,
+	SIMPLE,
+	MEDIUM,
+	COMPLEX,
+	ADAPTIVE
+}	t_algorithm;
+
+#endif
