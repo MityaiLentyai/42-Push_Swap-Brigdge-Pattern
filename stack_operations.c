150d4892966ebfee0f1328b97c146e70b91e5757
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-07-08 21:55:36 +0200

    Fixed stack_operations' structure  according to new requirements + norme fixes

--- stack_operations.c
+++ stack_operations.c
@@ -1,64 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_operations.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzzayats <dzzayats@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/08 21:45:52 by dzzayats          #+#    #+#             */
+/*   Updated: 2026/07/08 21:45:52 by dzzayats         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 // PA: take the first element from the top of B and place it at the top of A.
 // Do nothing if B is empty.
-void	PA(t_state *state, int counter)
+
+void	pa(t_state *state, int counter)
 {
 	t_dlist	*tmp;
 
 	if (!counter || !state)
 		return ;
-	if (!(state->head_b)||(!(state->head_b)))
+	if (!(state->stack_b->head))
 		return ;
-	tmp = lst_release_front(state,&state->head_b , &state->tail_b);
+	tmp = lst_release_front(state, &state->stack_b->head,
+			&state->stack_b->head);
 	if (!tmp)
 		return ;
-	lst_add_front(state, tmp, &state->head_a, &state->tail_a);
+	lst_add_front(state, tmp, &state->stack_a->head, &state->stack_a->tail);
+	write(1, "PA\n", 3);
+	state->benchmark->pa++;
 }
 
 // PB Take the first element at the top of a and put it ast the top of b.
-// Do nothing if a is empty. 
-void PB(t_state *state, int counter)
+// Do nothing if a is empty.
+
+void	pb(t_state *state, int counter)
 {
 	t_dlist	*tmp;
 
 	if (!counter || !state)
 		return ;
-	if (!(state->head_a)||(!(state->head_b)))
+	if (!(state->stack_a->head))
 		return ;
-	tmp = lst_release_front(state,&state->head_a , &state->tail_a);
+	tmp = lst_release_front(state, &state->stack_a->head,
+			&state->stack_a->tail);
 	if (!tmp)
 		return ;
-	lst_add_front(state,tmp,&state->head_b,&state->tail_b);
+	lst_add_front(state, tmp, &state->stack_b->head, &state->stack_b->tail);
+	write(1, "PB\n", 3);
+	state->benchmark->pb++;
 }
 // RA (rotate a): Shift up all elements of stack a by one.
-// The first element becomes the last one. 
-void RA(t_state *state, int counter)
+// The first element becomes the last one.
+
+void	ra(t_state *state, int counter)
 {
-        t_dlist *tmp;
-        
-        if (!counter || !state)
+	t_dlist	*tmp;
+
+	if (!counter || !state)
 		return ;
-	if (!(state->head_a)||(!(state->tail_a)))
+	if (!(state->stack_a->head) || (!(state->stack_a->tail)))
 		return ;
-        tmp = lst_release_front(state,&state->head_a,&state->tail_a);
-        if (!tmp)
-                return  ;
-        lst_add_back(state,tmp,&state->head_a,&state->tail_a); 
+	tmp = lst_release_front(state, &state->stack_a->head,
+			&state->stack_a->tail);
+	if (!tmp)
+		return ;
+	lst_add_back(state, tmp, &state->stack_a->head, &state->stack_a->tail);
+	write(1, "RA\n", 3);
+	state->benchmark->ra++;
 }
 
-// RRA (reverse rotate a): Shift down all elements of stack a by one. 
+// RRA (reverse rotate a): Shift down all elements of stack a by one.
 // The last element becomes the first one.
-void RRA(t_state *state, int counter)
+
+void	rra(t_state *state, int counter)
 {
-        t_dlist *tmp;
-        
-        if (!counter || !state)
+	t_dlist	*tmp;
+
+	if (!counter || !state)
 		return ;
-	if (!(state->head_a)||(!(state->tail_a)))
+	if (!(state->stack_a->head) || (!(state->stack_a->tail)))
 		return ;
-        tmp = lst_release_back(state,&state->head_a,&state->tail_a);
-        if (!tmp)
-                return  ;
-        lst_add_front(state,tmp,&state->head_a,&state->tail_a); 
+	tmp = lst_release_back(state, &state->stack_a->head, &state->stack_a->tail);
+	if (!tmp)
+		return ;
+	lst_add_front(state, tmp, &state->stack_a->head, &state->stack_a->tail);
+	write(1, "RRA\n", 3);
+	state->benchmark->rra++;
 }
