19897811bc3bfe7b42d9ce4a941a854647bdc627
Natalia <nataliasuszanowicz@gmail.com>
2026-07-08 20:51:21 +0200

    Simple finished

--- simple.c
+++ simple.c
@@ -6,22 +6,23 @@
 
 void	simple(t_state *state)
 {
-	int	stack_a_size;
+	int	stack_size;
 	int	min_idx;
 	int how_many_op;
 
-	stack_a_size = list_size(state->stack_a);
-	while (stack_a_size > 2)
+	stack_size = list_size(state->head_a);
+	while (stack_size > 2)
 	{
-		min_idx = idx_of_min(state->stack_a);
-		if (min_idx > (stack_a_size - 1) / 2)
-			RRA(state, stack_a_size - min_idx);
+		min_idx = idx_of_min(state->head_a);
+		if (min_idx > (stack_size - 1) / 2)
+			RRA(state, stack_size - min_idx);
 		else
 			RA(state, min_idx);
 		PA(state, 1);
-		stack_a_size--;
+		stack_size--;
 	}
-	if (check_if_swap(state->stack_a))
+	if (check_if_swap(state->head_a))
 		SA(state, 1);
-	stack_a_size =
+	stack_size = list_size(state->head_b);
+	PA(state, stack_size);
 }
